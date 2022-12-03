a5ff4bd53488310866cee58edeb587f94d1c6f06
vishrs <vishal.vemula@capitalone.com>
2022-12-02 22:01:13 -0500

    day 2 full solution

--- day2/day2.py
+++ day2/day2.py
@@ -1,22 +1,25 @@
 def read_strategy():
     
-    winning_hand = {'A':'Y', 'B':'Z', 'C':'X'}
-    draw_hand = {'A':'X', 'B':'Y', 'C':'Z'}
+    winning_hand = {'A':'P', 'B':'S', 'C':'R'}
+    draw_hand = {'A':'R', 'B':'P', 'C':'S'}
+    lose_hand = {'A':'S', 'B':'R', 'C':'P'}
+    round_scoring = {'Y':3, 'X':0, 'Z':6}
+    play_scoring = {'R':1,'P':2,'S':3}
 
-    score = {'Y':2, 'X':1, 'Z':3}
     total = 0
     with open('inputs.txt') as inputs:
 
         for line in inputs:
             round = line.strip().split(' ')
             
-            if winning_hand[round[0]] == round[1]:
-                # won
-                total += 6
-            elif draw_hand[round[0]] == round[1]:
-                total += 3
+            if round[1] == 'Z':
+                total += play_scoring[winning_hand[round[0]]]
+            elif round[1] == 'Y':
+                total += play_scoring[draw_hand[round[0]]]
+            else:
+                total += play_scoring[lose_hand[round[0]]]
 
-            total += score[round[1]]
+            total += round_scoring[round[1]]
 
     return total
 
